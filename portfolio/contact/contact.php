ac9aca16b0f01015fb786b603edf76703a68d14c
OumaESS <57940173+OumaESS@users.noreply.github.com>
2020-09-13 14:17:12 +0100

    modification dans le fichier contact

--- portfolio/contact/contact.php
+++ portfolio/contact/contact.php
@@ -6,12 +6,12 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     $email = $_POST['email'];
     $message = $_POST['message'];
     
-    $email_from = 'oumaimaessafir@gmail.com';
+    $email_from = 'oumaess959@gmail.com';
     $email_subject ="contact form:  $name";
     $email_body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\nEmail: $email\n\nMessage:\n$message";
     
     
-    $to = 'oumaimaessafir@gmail.com';
+    $to = 'oumaess959@gmail.com';
     $headers = "form : $email_from \r\n ";
     $headers .= "Reply-to : $email \r\n";
     mail($to, $email_subject,$email_body,$headers);
