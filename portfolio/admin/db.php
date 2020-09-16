cdea59bc7d1831774455b226742579a9cc63fd07
OumaESS <57940173+OumaESS@users.noreply.github.com>
2020-09-16 18:41:47 +0100

    basse de données

--- portfolio/admin/db.php
+++ portfolio/admin/db.php
@@ -1,14 +1,14 @@
-<?php 
+<?php
 $localName = 'localhost';
-$dbname = 'portfolio';
-$user = 'root';
-$pwd = '';
+$db_name = 'portfolio';
+$db_user = 'root';
+$db_mdp = '';
+$db_cnx = 'mysql:host=localhost;dbname=portfolio';
 try{
-    $db = new PDO('mysql:host='.$localName.';dbname='.$dbname,$user,$pwd);
-    $db->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE,PDO::FETCH_ASSOC);
-    $db->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_WARNING);
-}catch(Exception $e){
-    echo('Impossible de conneceter à la base de donnee');
-    echo($e->getMessage());
-    die;
+$db_connect= new PDO($db_cnx,$db_user,$db_mdp);
+$db_connect->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch(PDOException $e){
+die('Error '.$e->getMessage());
 }
+?>
+
